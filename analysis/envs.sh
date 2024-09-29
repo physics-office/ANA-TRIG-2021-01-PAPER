0a15b3c2ddc950931ba3020308c00048eac26b07
Joao Victor Da Fonseca Pinto <joao.pinto@caloba70.lps.ufrj.br>
2024-09-28 21:46:21 -0300

    update

--- analysis/envs.sh
+++ analysis/envs.sh
@@ -2,7 +2,7 @@
 export DOCKER_NAMESPACE=$USER
 export DOCKER_IMAGES="/mnt/cern_data/$USER/images"
 
-export VIRTUALENV_NAMESPACE="paper-env"
+export VIRTUALENV_NAMESPACE=".paper-env"
 
 export CERN_DATA="/mnt/cern_data/joao.pinto"
 export PYTHONPATH=$PWD:$PYTHONPATH
